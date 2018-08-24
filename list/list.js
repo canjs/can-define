cf867364df9e8e4d187a6920289123ba52fd05dd
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-08-24 13:44:18 -0700

    filter documentation moved to docs/prototype.filter.md. ES6 syntax highlighting, they're codepen-able.

--- list/list.js
+++ list/list.js
@@ -434,50 +434,6 @@ canReflect.eachKey({
 canReflect.eachKey({
 	"map": 3,
 	/**
-	 * @function can-define/list/list.prototype.filter filter
-	 *
-	 * Filter a list to a new list of the matched items.
-	 *
-	 * @signature `list.filter( callback [,thisArg] )`
-	 *
-	 * Filters `list` based on the return value of `callback`.
-	 *
-	 * ```
-	 * var names = new DefineList(["alice","adam","zack","zeffer"]);
-	 * var aNames = names.filter(function(name){
-	 *   return name[0] === "a"
-	 * });
-	 * aNames //-> DefineList["alice","adam"]
-	 * ```
-	 *
-	 *   @param  {function(*, Number, can-define/list/list)} callback(item, index, list) A
-	 *   function to call with each element of the DefineList. The three parameters that callback gets passed are:
-	 *    - item (*) - the element at index.
-	 *    - index (Integer) - the index of the current element of the list.
-	 *    - list (DefineList) - the `DefineList` the elements are coming from.
-	 *
-	 *   If `callback` returns a truthy result, `item` will be added to the result.  Otherwise, the `item` will be
-	 *   excluded.
-	 *
-	 *   @param  {Object}  thisArg  What `this` should be in the `callback`.
-	 *   @return {can-define/list/list} A new instance of this `DefineList` (may be a subclass), containing the items that passed the filter.
-	 *
-	 * @signature `list.filter( props )`
-	 *
-	 * Filters items in `list` based on the property values in `props`.
-	 *
-	 * ```
-	 * var todos = new DefineList([
-	 *   {name: "dishes", complete: false},
-	 *   {name: "lawn", complete: true}
-	 * ]);
-	 * var complete = todos.filter({complete: true});
-	 * complete //-> DefineList[{name: "lawn", complete: true}]
-	 * ```
-	 *
-	 *    @param  {Object}  props An object of key-value properties.  Each key and value in
-	 *    `props` must be present on an `item` for the `item` to be in the returned list.
-	 *    @return {can-define/list/list} A new `DefineList` of the same type.
 	 */
 	"filter": 3,
 	/**
