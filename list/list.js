ab7c4c28e8533a909f25e20bfbf59e3211d7e02d
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-08-24 14:26:33 -0700

    documentation moved to docs/prototype.some.md. ES6 syntax, highlighting and corrected the comments.

--- list/list.js
+++ list/list.js
@@ -437,54 +437,6 @@ canReflect.eachKey({
 	"reduce": 4,
 	"reduceRight": 4,
 	"every": 3,
-	/**
-	 * @function can-define/list/list.prototype.some some
-	 *
-	 * Return true if at least one item in a list matches a predicate.
-	 *
-	 * @signature `list.some( callback [,thisArg] )`
-	 *
-	 * Tests each item in `list` by calling `callback` on it.  If `callback` returns truthy for some element in
-	 * `list`, `some` returns `true`.
-	 *
-	 * ```
-	 * var names = new DefineList(["alice","adam","zack","zeffer"]);
-	 * var aNames = names.some(function(name){
-	 *   return name[0] === "a"
-	 * });
-	 * aNames //-> false
-	 * ```
-	 *
-	 *   @param  {function(*, Number, can-define/list/list)} callback(item, index, list) A
-	 *   function to call with each element of the DefineList. The three parameters that callback gets passed are:
-	 *    - item (*) - the element at index.
-	 *    - index (Integer) - the index of the current element of the list.
-	 *    - list (DefineList) - the DefineList the elements are coming from.
-	 *
-	 *   If `callback` returns a falsy result, `some` will evaluate the callback on the next element.  Otherwise, `some`
-	 *   will return `true`.
-	 *
-	 *   @param  {Object}  thisArg  What `this` should be in the `callback`.
-	 *   @return {Boolean} `false` if calling the callback on some element in `list` returns a falsy value, `true` otherwise.
-	 *
-	 * @signature `list.some( props )`
-	 *
-	 * Tests each item in `list` by comparing its properties to `props`.  If `props` match for some element in
-	 * `list`, `some` returns `true`.
-	 *
-	 * ```
-	 * var todos = new DefineList([
-	 *   {name: "dishes", complete: false},
-	 *   {name: "lawn", complete: true}
-	 * ]);
-	 * var complete = todos.some({complete: true});
-	 * complete //-> false
-	 * ```
-	 *
-	 *    @param  {Object}  props An object of key-value properties.  Each key and value in
-	 *    `props` must be present on an `item` for the `item` to match.
-	 *    @return {Boolean} `false` if every element in `list` fails to match `props`, `true` otherwise
-	 */
 	"some": 3
 },
 function a(fnLength, fnName) {
