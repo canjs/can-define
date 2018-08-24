c4f3919d8947e5cecc3c430105d33ae619753f28
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-08-24 14:16:01 -0700

    documentation moved to docs/prototype.every.md. Updated syntax to ES6 added syntax highlighting. Links to codepen.

--- list/list.js
+++ list/list.js
@@ -436,54 +436,6 @@ canReflect.eachKey({
 	"filter": 3,
 	"reduce": 4,
 	"reduceRight": 4,
-	/**
-	 * @function can-define/list/list.prototype.every every
-	 *
-	 * Return true if every item in a list matches a predicate.
-	 *
-	 * @signature `list.every( callback [,thisArg] )`
-	 *
-	 * Tests each item in `list` by calling `callback` on it.  If `callback` returns truthy for every element in
-	 * `list`, `every` returns `true`.
-	 *
-	 * ```
-	 * var names = new DefineList(["alice","adam","zack","zeffer"]);
-	 * var aNames = names.every(function(name){
-	 *   return name[0] === "a"
-	 * });
-	 * aNames //-> false
-	 * ```
-	 *
-	 *   @param  {function(*, Number, can-define/list/list)} callback(item, index, list) A
-	 *   function to call with each element of the DefineList. The three parameters that callback gets passed are:
-	 *    - item (*) - the element at index.
-	 *    - index (Integer) - the index of the current element of the list.
-	 *    - list (DefineList) - the `DefineList` the elements are coming from.
-	 *
-	 *   If `callback` returns a truthy result, `every` will evaluate the callback on the next element.  Otherwise, `every`
-	 *   will return `false`.
-	 *
-	 *   @param  {Object}  thisArg  What `this` should be in the `callback`.
-	 *   @return {Boolean} `true` if calling the callback on every element in `list` returns a truthy value, `false` otherwise.
-	 *
-	 * @signature `list.every( props )`
-	 *
-	 * Tests each item in `list` by comparing its properties to `props`.  If `props` match for every element in
-	 * `list`, `every` returns `true`.
-	 *
-	 * ```
-	 * var todos = new DefineList([
-	 *   {name: "dishes", complete: false},
-	 *   {name: "lawn", complete: true}
-	 * ]);
-	 * var complete = todos.every({complete: true});
-	 * complete //-> false
-	 * ```
-	 *
-	 *    @param  {Object}  props An object of key-value properties.  Each key and value in
-	 *    `props` must be present on an `item` for the `item` to match.
-	 *    @return {Boolean} `true` if every element in `list` matches `props`, `false` otherwise.
-	 */
 	"every": 3,
 	/**
 	 * @function can-define/list/list.prototype.some some
