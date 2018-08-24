a40384e6753a8a98219a3dd7d987d2aa9ad3a3fb
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-08-24 13:53:53 -0700

    reduce documentation moved to docs/prototype.reduce.md. ES6 syntax and codepen-able.

--- list/list.js
+++ list/list.js
@@ -435,42 +435,6 @@ canReflect.eachKey({
 	"map": 3,
 	"filter": 3,
 	/**
-	 * @function can-define/list/list.prototype.reduce reduce
-	 * @description Map the values in this list to a single value
-	 *
-	 * @signature `list.reduce(callback, initialValue, [, thisArg])`
-	 *
-	 * Loops through the values of the list, calling `callback` for each one until the list
-	 * ends.  The return value of `callback` is passed to the next iteration as the first argument,
-	 * and finally returned by `reduce`.
-	 *
-	 * ```js
-	 * var todos = new DefineList([
-	 *   {name: "dishes", complete: false},
-	 *   {name: "lawn", complete: true}
-	 * ]);
-	 * var todosAsOneObject = todos.reduce(function(todos, todo){
-	 *   todos[todo.name] = todo.complete;
-	 *   return todos;
-	 * }, {});
-	 * todosAsOneObject //-> { dishes: false, lawn: true }
-	 * ```
-	 *
-	 * @param {function(item, index, list)} callback A function to call with each element of the DefineList.
-	 * The four parameters that callback gets passed are:
-	 *    - current (*) - the current aggregate value of reducing over the list -- the initial value if the first iteration
-	 *    - item (*) - the element at index.
-	 *    - index (Integer) - the index of the current element of the list.
-	 *    - list (DefineList) - the `DefineList` the elements are coming from.
-	 *
-	 * The return value of `callback` is passed to the next iteration as the first argument, and returned from
-	 * `reduce` if the last iteration.
-	 *
-	 * @param {*} [initialValue] The initial value to use as `current` in the first iteration
-	 * @param {Object} [thisArg] The object to use as `this` inside the callback.
-	 * @return {*} The result of the final call of `callback` on the list.
-	 * @body
-	 *
 	 */
 	"reduce": 4,
 	/**
