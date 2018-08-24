a81be1050f343f090a5e0856a92050e0c0fc9120
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-08-24 15:42:00 -0700

    sort documentation moved to docs/prototype.sort.md. Examples are highlighted and ES6. It's codepenable. Updated example to better relate to similarities across the documentation.

--- list/list.js
+++ list/list.js
@@ -552,50 +552,6 @@ assign(DefineList.prototype, {
 
 		return this;
 	},
-	/**
-	 * @function can-define/list/list.prototype.sort sort
-	 * @description Sort the properties of a list.
-	 *
-	 * @signature `list.sort([compareFunction])`
-	 *
-	 * Sorts the elements of a list in place and returns the list. The API is the
-	 * same as the native JavaScript `Array.prototype.sort` API.
-	 *
-	 * ```js
-	 * var accounts = new Account.List([
-	 *   { name: "Savings", amount: 20.00 },
-	 *   { name: "Checking", amount: 103.24 },
-	 *   { name: "Kids Savings", amount: 48155.13 }
-	 * ]);
-	 * accounts.sort(function(a, b){
-	 *   if (a.name < b.name) {
-	 *     return -1;
-	 *   } else if (a.name > b.name){
-	 *     return 1;
-	 *   } else {
-	 *     return 0;
-	 *   }
-	 * });
-	 * accounts[0].name === "Checking"
-	 * accounts[1].name === "Kids Savings"
-	 * accounts[2].name === "Savings"
-	 * ```
-	 *
-	 * @param {function(a, b)} compareFunction Specifies a function that defines the sort order.
-	 *
-	 * If `compareFunction` is supplied, the list elements are sorted according to the return
-	 * value of the compare function. If `a` and `b` are two elements being compared, then:
-	 *
-	 *  - If `compareFunction(a, b)` returns a value less than 0, `a` will be sorted to
-	 *  a lower index than `b`, so `a` will now come first.
-	 *  - If `compareFunction(a, b)` returns 0, the order of the two values will not be changed.
-	 *  - If `compareFunction(a, b)` returns a value greater than 0, `a` will be sorted to
-	 *  a higher index than `b`, so `b` will now come first.
-	 *
-	 * @return {can-define/list/list} The list instance.
-	 * @body
-	 * ```
-	 */
 	sort: function(compareFunction) {
 		var sorting = Array.prototype.slice.call(this);
 		Array.prototype.sort.call(sorting, compareFunction);
@@ -603,7 +559,6 @@ assign(DefineList.prototype, {
 		return this;
 	}
 });
-
 
 // Add necessary event methods to this object.
 for (var prop in define.eventsProto) {
