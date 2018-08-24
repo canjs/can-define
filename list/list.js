046e9810035a25beb66d07863d7ed531beab31bf
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-08-24 15:28:24 -0700

    replace documentation moved to docs/prototype.replace.md. examples are highlighted and using ES6 syntax. It's codepenable.

--- list/list.js
+++ list/list.js
@@ -538,33 +538,6 @@ assign(DefineList.prototype, {
 		}
 		return this;
 	},
-
-	/**
-	 * @function can-define/list/list.prototype.replace replace
-	 * @description Replace all the elements of a DefineList.
-	 * @signature `list.replace(collection)`
-	 *
-	 * Replaces every item in the list with `collection`.
-	 *
-	 * ```
-	 * var names = new DefineList(["alice","adam","eve"]);
-	 * names.replace(["Justin","Xena"]);
-	 * names //-> DefineList["Justin","Xena"]
-	 * ```
-	 *
-	 * @param {Array|can-define/list/list} collection The collection of items that will be in `list`.
-	 * @return {can-define/list/list} Returns the `list`.
-	 *
-	 * @body
-	 *
-	 * ## Use
-	 *
-	 * `replace` is essentially a shortcut for [can-define/list/list.prototype.splice].
-	 *
-	 * ## Events
-	 *
-	 * `replace` causes _remove_, _add_, and _length_ events.
-	 */
 	replace: function(newList) {
 		var patches = diff(this, newList);
 
